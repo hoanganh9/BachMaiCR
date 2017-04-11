c7adc09f2a5385e75769166a5cd5f2083a30ca39
hoanganh9 <mr.hoanganh.it@gmail.com>
2017-04-11 12:00:53 +0700

    Add project files.

---
+++ BachMaiCR.Web/Constants/Application.cs
@@ -0,0 +1,8 @@
+﻿namespace BachMaiCR.Web.Constants
+{
+    public class Application
+    {
+        public const string Name = "ASP.NET MVC Boilerplate";
+        public const string ShortName = "Boilerplate";
+    }
+}
---
+++ BachMaiCR.Web/Constants/ControllerName.cs
@@ -0,0 +1,8 @@
+﻿namespace BachMaiCR.Web.Constants
+{
+    public static class ControllerName
+    {
+        public const string Error = "Error";
+        public const string Home = "Home";
+    }
+}
